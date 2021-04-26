d44c3c361fad11e9338c5b3ea6d2be63056acd3b
chenyubo <chenyubo@didichuxing.com>
2021-04-26 23:07:10 +0800

    up

--- config.php
+++ config.php
@@ -6,8 +6,8 @@ return array(
             'time2' => 17
         ),
         'field' => array(
-            'num1' => 11,
-            'num2' => 12
+            'num1' => 8,
+            'num2' => 9
         ),
         'cookie' => 'Cookie: JSESSIONID=71603E36C21A7FE090EA17FA1E76E846; gr_session_id_ade9dc5496ada31e_b46306a0-255f-4fe2-8568-43331a4322ff=true; Hm_lpvt_bc864c0a0574a7cabe6b36d53206fb69=1619445865; Hm_lvt_bc864c0a0574a7cabe6b36d53206fb69=1618238109,1618758404,1619357906,1619445859; gr_session_id_ade9dc5496ada31e=b46306a0-255f-4fe2-8568-43331a4322ff; gr_user_id=5f2e352b-54b4-4ab7-82c6-bf88d2df5eaf'
     ),
@@ -17,8 +17,8 @@ return array(
             'time2' => 17
         ),
         'field' => array(
-            'num1' => 13,
-            'num2' => 14
+            'num1' => 10,
+            'num2' => 11
         ),
          'cookie' => 'Cookie: JSESSIONID=341A6D92278BEACE3F97125B7DC465E6; gr_session_id_ade9dc5496ada31e_927f242f-6703-468c-9e6b-d3c0774f4332=true; Hm_lpvt_bc864c0a0574a7cabe6b36d53206fb69=1619446494; Hm_lvt_bc864c0a0574a7cabe6b36d53206fb69=1618272136,1618758573,1618845683,1619446488; gr_session_id_ade9dc5496ada31e=927f242f-6703-468c-9e6b-d3c0774f4332; gr_user_id=68bef62a-ca17-46b0-a1cb-ae6626e520e6'
     ),
@@ -28,10 +28,10 @@ return array(
             'time2' => 17
         ),
         'field' => array(
-            'num1' => 9,
-            'num2' => 10
+            'num1' => 12,
+            'num2' => 13
         ),
-         'cookie' => 'Cookie: JSESSIONID=F75A95C4D51FA986BC546A8E4D0D5CFE; gr_session_id_ade9dc5496ada31e=0a726bbe-858d-402a-beac-4af1728c52c0; gr_session_id_ade9dc5496ada31e_0a726bbe-858d-402a-beac-4af1728c52c0=true; gr_user_id=0e223474-dcd8-42fc-8694-fec066985261; Hm_lpvt_bc864c0a0574a7cabe6b36d53206fb69=1619357685; Hm_lvt_bc864c0a0574a7cabe6b36d53206fb69=1619357680'
+         'cookie' => 'Cookie: JSESSIONID=18579D03E6A7E00987DEC1F6CEBDECD8; gr_session_id_ade9dc5496ada31e_b40041cb-3e30-4e5d-9e47-8f65130e4550=true; Hm_lpvt_bc864c0a0574a7cabe6b36d53206fb69=1619448611; Hm_lvt_bc864c0a0574a7cabe6b36d53206fb69=1618876930,1619143554,1619357739,1619448604; gr_session_id_ade9dc5496ada31e=b40041cb-3e30-4e5d-9e47-8f65130e4550; gr_user_id=f000d006-c580-4aa3-9da4-2f9fcdad1e21'
     ),
     array(
        'time' => array(
@@ -39,20 +39,20 @@ return array(
             'time2' => 17
         ),
         'field' => array(
-            'num1' => 8,
+            'num1' => 14,
+            'num2' => 15
+        ),
+         'cookie' => 'Cookie: JSESSIONID=A49723117249858D470AE60B9E8FD114; Hm_lpvt_bc864c0a0574a7cabe6b36d53206fb69=1619448710; Hm_lvt_bc864c0a0574a7cabe6b36d53206fb69=1619448648; gr_user_id=f676c39e-86e8-4f97-b9ac-e1f8cbc9955a; gr_session_id_ade9dc5496ada31e_c99b8c82-6450-4c59-896c-750282b47027=true; gr_session_id_ade9dc5496ada31e=c99b8c82-6450-4c59-896c-750282b47027'
+    ),
+    array(
+       'time' => array(
+            'time1' => 16,
+            'time2' => 17
+        ),
+        'field' => array(
+            'num1' => 6,
             'num2' => 7
         ),
-         'cookie' => 'Cookie: JSESSIONID=49EE5D1412EC15F2E9D5A9094820EA88; Hm_lpvt_bc864c0a0574a7cabe6b36d53206fb69=1619357755; Hm_lvt_bc864c0a0574a7cabe6b36d53206fb69=1618842790,1618876930,1619143554,1619357739; gr_session_id_ade9dc5496ada31e=9aaa7723-d1d9-4a96-9e3c-be883c3dad95; gr_session_id_ade9dc5496ada31e_9aaa7723-d1d9-4a96-9e3c-be883c3dad95=true; gr_user_id=f000d006-c580-4aa3-9da4-2f9fcdad1e21'
-    ),
-    array(
-       'time' => array(
-            'time1' => 16,
-            'time2' => 17
-        ),
-        'field' => array(
-            'num1' => 11,
-            'num2' => 12
-        ),
-         'cookie' => ''
+         'cookie' => 'Cookie: JSESSIONID=5D1A059A87C7FB0C4ECDFEB075D7AAA5; gr_session_id_ade9dc5496ada31e=b0f90cfa-1265-4433-808a-6bac54167e03; gr_session_id_ade9dc5496ada31e_b0f90cfa-1265-4433-808a-6bac54167e03=true; gr_user_id=48c209b6-5a28-4503-8f7a-53c763531158; Hm_lpvt_bc864c0a0574a7cabe6b36d53206fb69=1619448791; Hm_lvt_bc864c0a0574a7cabe6b36d53206fb69=1619448782'
     ),
 );
