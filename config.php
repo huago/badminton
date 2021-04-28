0ca96a3a730e70ba30a36a1dde146a04bf833e20
chenyubo <chenyubo@didichuxing.com>
2021-04-28 23:25:42 +0800

    up

--- config.php
+++ config.php
@@ -2,57 +2,57 @@
 return array(
     array(
         'time' => array(
-            'time1' => 16,
-            'time2' => 17
+            'time1' => 19,
+            'time2' => 20
         ),
         'field' => array(
             'num1' => 8,
             'num2' => 9
         ),
-        'cookie' => 'Cookie: JSESSIONID=71603E36C21A7FE090EA17FA1E76E846; gr_session_id_ade9dc5496ada31e_b46306a0-255f-4fe2-8568-43331a4322ff=true; Hm_lpvt_bc864c0a0574a7cabe6b36d53206fb69=1619445865; Hm_lvt_bc864c0a0574a7cabe6b36d53206fb69=1618238109,1618758404,1619357906,1619445859; gr_session_id_ade9dc5496ada31e=b46306a0-255f-4fe2-8568-43331a4322ff; gr_user_id=5f2e352b-54b4-4ab7-82c6-bf88d2df5eaf'
+        'cookie' => 'Cookie: JSESSIONID=EA470BFCD4C4EC2119DA28870F49A266; Hm_lpvt_bc864c0a0574a7cabe6b36d53206fb69=1619622809; Hm_lvt_bc864c0a0574a7cabe6b36d53206fb69=1619482038,1619622804; gr_session_id_ade9dc5496ada31e=e046c36a-3540-466e-8ac0-40c9573dd1bd; gr_session_id_ade9dc5496ada31e_e046c36a-3540-466e-8ac0-40c9573dd1bd=true; gr_user_id=4a1d01d5-b7db-49d9-8770-1887719c2910'
     ),
     array(
        'time' => array(
-            'time1' => 16,
-            'time2' => 17
+            'time1' => 19,
+            'time2' => 20
         ),
         'field' => array(
             'num1' => 10,
             'num2' => 11
         ),
-         'cookie' => 'Cookie: JSESSIONID=341A6D92278BEACE3F97125B7DC465E6; gr_session_id_ade9dc5496ada31e_927f242f-6703-468c-9e6b-d3c0774f4332=true; Hm_lpvt_bc864c0a0574a7cabe6b36d53206fb69=1619446494; Hm_lvt_bc864c0a0574a7cabe6b36d53206fb69=1618272136,1618758573,1618845683,1619446488; gr_session_id_ade9dc5496ada31e=927f242f-6703-468c-9e6b-d3c0774f4332; gr_user_id=68bef62a-ca17-46b0-a1cb-ae6626e520e6'
+         'cookie' => 'Cookie: JSESSIONID=D4B67F25C9BE2080FB143DF4E5C8FD52; Hm_lpvt_bc864c0a0574a7cabe6b36d53206fb69=1619622815; Hm_lvt_bc864c0a0574a7cabe6b36d53206fb69=1619143554,1619357739,1619448604,1619622800; gr_session_id_ade9dc5496ada31e=5ef60dff-934b-469e-ab20-da0020ff9b28; gr_session_id_ade9dc5496ada31e_5ef60dff-934b-469e-ab20-da0020ff9b28=true; gr_user_id=f000d006-c580-4aa3-9da4-2f9fcdad1e21'
     ),
     array(
         'time' => array(
-            'time1' => 16,
-            'time2' => 17
+            'time1' => 19,
+            'time2' => 20
         ),
         'field' => array(
             'num1' => 12,
             'num2' => 13
         ),
-         'cookie' => 'Cookie: JSESSIONID=18579D03E6A7E00987DEC1F6CEBDECD8; gr_session_id_ade9dc5496ada31e_b40041cb-3e30-4e5d-9e47-8f65130e4550=true; Hm_lpvt_bc864c0a0574a7cabe6b36d53206fb69=1619448611; Hm_lvt_bc864c0a0574a7cabe6b36d53206fb69=1618876930,1619143554,1619357739,1619448604; gr_session_id_ade9dc5496ada31e=b40041cb-3e30-4e5d-9e47-8f65130e4550; gr_user_id=f000d006-c580-4aa3-9da4-2f9fcdad1e21'
+         'cookie' => 'Cookie: JSESSIONID=F5569EE8CEEA83215AC858923FC4EF52; gr_session_id_ade9dc5496ada31e_bd46f71c-626c-4c45-bed2-553d05f9825d=true; Hm_lpvt_bc864c0a0574a7cabe6b36d53206fb69=1619621605; Hm_lvt_bc864c0a0574a7cabe6b36d53206fb69=1618758404,1619357906,1619445859,1619621595; gr_session_id_ade9dc5496ada31e=bd46f71c-626c-4c45-bed2-553d05f9825d; gr_user_id=5f2e352b-54b4-4ab7-82c6-bf88d2df5eaf'
     ),
     array(
        'time' => array(
-            'time1' => 16,
-            'time2' => 17
+            'time1' => 19,
+            'time2' => 20
         ),
         'field' => array(
             'num1' => 14,
             'num2' => 15
         ),
-         'cookie' => 'Cookie: JSESSIONID=A49723117249858D470AE60B9E8FD114; Hm_lpvt_bc864c0a0574a7cabe6b36d53206fb69=1619448710; Hm_lvt_bc864c0a0574a7cabe6b36d53206fb69=1619448648; gr_user_id=f676c39e-86e8-4f97-b9ac-e1f8cbc9955a; gr_session_id_ade9dc5496ada31e_c99b8c82-6450-4c59-896c-750282b47027=true; gr_session_id_ade9dc5496ada31e=c99b8c82-6450-4c59-896c-750282b47027'
+         'cookie' => 'Cookie: JSESSIONID=997CA6830E4B8A51EB3C93AA1A4640CC; Hm_lpvt_bc864c0a0574a7cabe6b36d53206fb69=1619621703; Hm_lvt_bc864c0a0574a7cabe6b36d53206fb69=1618272136,1618758573,1618845683,1619446488; gr_session_id_ade9dc5496ada31e=285c5b68-270b-4045-89e8-28898abd94e8; gr_session_id_ade9dc5496ada31e_285c5b68-270b-4045-89e8-28898abd94e8=true; gr_user_id=68bef62a-ca17-46b0-a1cb-ae6626e520e6'
     ),
     array(
        'time' => array(
-            'time1' => 16,
-            'time2' => 17
+            'time1' => 19,
+            'time2' => 20
         ),
         'field' => array(
             'num1' => 6,
             'num2' => 7
         ),
-         'cookie' => 'Cookie: JSESSIONID=5D1A059A87C7FB0C4ECDFEB075D7AAA5; gr_session_id_ade9dc5496ada31e=b0f90cfa-1265-4433-808a-6bac54167e03; gr_session_id_ade9dc5496ada31e_b0f90cfa-1265-4433-808a-6bac54167e03=true; gr_user_id=48c209b6-5a28-4503-8f7a-53c763531158; Hm_lpvt_bc864c0a0574a7cabe6b36d53206fb69=1619448791; Hm_lvt_bc864c0a0574a7cabe6b36d53206fb69=1619448782'
+         'cookie' => 'Cookie: JSESSIONID=680ACF093B57EF2BECE37813AE072BE0; gr_session_id_ade9dc5496ada31e=59a2e807-31f5-4580-bb71-ad85bd39515b; gr_session_id_ade9dc5496ada31e_59a2e807-31f5-4580-bb71-ad85bd39515b=true; gr_user_id=6814bc2e-97b1-412a-8702-7d47b6f7bdc7; Hm_lpvt_bc864c0a0574a7cabe6b36d53206fb69=1619623088; Hm_lvt_bc864c0a0574a7cabe6b36d53206fb69=1619623082'
     ),
 );
